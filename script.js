38bdcfd7333867e340e956777c57e6b9d69e056f
Navdeshsingh.saini <navdeshsinghsaini@gmail.com>
2023-02-19 17:50:58 +0530

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let btn = document.getElementById("swap");
+let main = document.getElementById("app");
+
+btn.addEventListener('click' , swapTheme)
+
+function swapTheme () {
+    btn.classList.replace("button_day" , "button_night");
+    main.classList.replace("day" ,"night") 
+}
+
 
